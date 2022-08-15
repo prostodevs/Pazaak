fd3dfebc036e7fd0fdd4c6956efb779db3698a53
justin <prostodevs@gmail.com>
2022-08-15 15:53:50 -0400

    Added algorithm to Game.java for gameplay calculations

--- src/com/pazaak/game/Game.java
+++ src/com/pazaak/game/Game.java
@@ -1,8 +1,14 @@
 package com.pazaak.game;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Game {
     boolean play = false;
     boolean rules = false;
+    private List<Integer> sideDeck = new ArrayList<>();
+    private final Scanner scanner = new Scanner(System.in); // console input
 
     //business methods
     public void execute() {
@@ -11,31 +17,58 @@ public class Game {
         welcome();
 
         if (play) {
+            // TODO: MainDeck Randomized (via method) BEFORE gameplay
+
+            // TODO: select SideDeck
+
             while (GameWatcher.getPlayerSetWinCount() != 2 || GameWatcher.getDealerSetWinCount() != 2) {
+                int playerIndex = GameWatcher.getTurnCount();
+                int dealerIndex = GameWatcher.getTurnCount() + 1;
+                boolean playerStand = false;
+                boolean dealerStand = false;
 
-                if (GameWatcher.getPlayerCardValue() < 20) {
-                    // stay
-                    // skipTurn
-                    // playSideCard
-                } else if (GameWatcher.getPlayerCardValue() > 20 && GameWatcher.getDealerCardValue() > 20) {
-                    // DO NOTHING, TIED SET
-                } else if (GameWatcher.getPlayerCardValue() > 20) {
-                    GameWatcher.setDealerSetWinCount(+1);
+                while (playerStand) {
+                    if (GameWatcher.getPlayerCardValue() < 20) {
+                        // Drawn card = Math.rand(MainDeck[playerIndex])
+                        System.out.println("Please select what you would like to do:\n" +
+                                "1. Stand\n" +
+                                "2. Skip Turn\n" +
+                                "3. Play a Side Card");
+                        String input = scanner.nextLine().trim();
+                        switch (input) {
+                            case "1":
+                                break;
+                            case "2":
+                                break;
+                            case "3":
+                                System.out.println(sideDeck);
+                        }
+                        // stand
+                        // skipTurn
+                        // playSideCard
+                    } else if (GameWatcher.getPlayerCardValue() > 20 && GameWatcher.getDealerCardValue() > 20) {
+                        // DO NOTHING, TIED SET
+                    } else if (GameWatcher.getPlayerCardValue() > 20) {
+                        GameWatcher.setDealerSetWinCount(+1);
+                    }
                 }
+                while (dealerStand) {
+                    if (GameWatcher.getDealerCardValue() < 20) { // AI will have set rules with Math.rand to force chance
+                        // stand
+                        // skipTurn
+                        // playSideCard
+                    } else if (GameWatcher.getPlayerCardValue() > 20 && GameWatcher.getDealerCardValue() > 20) {
+                        // DO NOTHING, TIED SET
+                    } else if (GameWatcher.getDealerCardValue() > 20) {
+                        // add win to player win count
+                        GameWatcher.setPlayerSetWinCount(+1);
+                    }
 
-                if (GameWatcher.getDealerCardValue() < 20) { // AI will have set rules with Math.rand to force chance
-                    // stay
-                    // skipTurn
-                    // playSideCard
-                } else if (GameWatcher.getPlayerCardValue() > 20 && GameWatcher.getDealerCardValue() > 20) {
-                    // DO NOTHING, TIED SET
-                } else if (GameWatcher.getDealerCardValue() > 20) {
-                    // add win to player win count
-                    GameWatcher.setPlayerSetWinCount(+1);
+                    GameWatcher.setTurnCount(+1);
+                    playerIndex += 1;
+                    dealerIndex += 1;
                 }
-                GameWatcher.setTurnCount(+1);
             }
-
         }
 
         if (rules) {
