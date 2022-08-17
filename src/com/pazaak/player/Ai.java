098aaf671940932051ef64dae390e1e34269e46d
justin <prostodevs@gmail.com>
2022-08-17 18:35:41 -0400

    updated UML and user class

--- src/com/pazaak/player/Ai.java
+++ src/com/pazaak/player/Ai.java
@@ -1,24 +1,11 @@
 package com.pazaak.player;
 
 import com.pazaak.deck.Card;
-import com.pazaak.deck.CardValues;
 
-import com.pazaak.deck.MainDeck;
-
-import java.util.ArrayList;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.List;
 
-
 public class Ai extends Player {
-
-
-    // Fields
-    private CardValues sideDeck;
-    private int choice;
-    private boolean playerStand = false;
-    private boolean dealerStand = false;
-    List<CardValues> sideCards;
-
 
     //ctor
     public Ai(List<Card> hand) {
@@ -28,47 +15,17 @@ public class Ai extends Player {
     // Methods
     public void play(String... option) {
         drawCard();
+        System.out.println("COMPUTER TOTAL: " + getCardValue());
+        System.out.println("COMPUTER COUNT: " + getCardCount());
+        stand();
 
     }
 
-    public int getChoice() {
-        if (GameWatcher.getDealerCardValue() <= 13) {
-            choice = 2;
-            System.out.println("Skip");
-        } else if (GameWatcher.getDealerCardValue() > 14 && GameWatcher.getDealerCardValue() < 20) {
-            // for each loop that will iterate through the 4 cards to determine value
-            sideCards = new ArrayList<CardValues>();
 
-            for (CardValues sideCard : sideCards) {
-                if (sideCards + GameWatcher.getDealerCardValue()){
-                    playSideCard();
-                }
-
-            }
-
-
-
-
-
-//            int cardValueOnBoard = sum(GameWatcher.getDealerCardValue() + sideDeck.getCardValue());
-//            if (cardValueOnBoard <= 0) {
-//                choice = ;
-//
-//            }
-
-        }
-
-
-        return 0;
-    }
-
-
-
-
-        @Override
+    @Override
     public void playSideCard() {
 
-        }
+    }
 
 
 }
